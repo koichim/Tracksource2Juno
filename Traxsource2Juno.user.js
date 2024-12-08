f81193345dde06f0dc09e1b68e2dd592f87bac7d
koichim <koichi@masuda.sppd.ne.jp>
2024-12-08 12:54:46 +0900

    fixed chart title

--- Traxsource2Juno.user.js
+++ Traxsource2Juno.user.js
@@ -269,7 +269,8 @@
                 let filename = the_chart.date + "_" + the_chart.chart_artist.cleansing() + "_" + the_chart.chart_title.cleansing() + ".json";
                 let encoded_chart = encodeURIComponent(JSON.stringify(the_chart, null, 2));
                 $("h1.title").prepend(" (<a id='download_txtfile' download='"+filename+"'>json</a>) ");
-                $("h1.title").append($("- <img src=\"https://wwwcdn.junodownload.com/14020302/images/digital/icons/favicon-32x32.png\" "+
+                $("h1.title").append(" - ");
+                $("h1.title").append($("<img src=\"https://wwwcdn.junodownload.com/14020302/images/digital/icons/favicon-32x32.png\" "+
                                        "alt=\"jd\" width=\"24\" height=\"24\" style=\"vertical-align: bottom;\"/>").on("click", function(){
                     juno_search_links.forEach(function(href, i, array){
                         let defer_time = (array.length - i)*1000
