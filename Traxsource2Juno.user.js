e9311df5ac1936df934476afa84ce5ee48b9c18d
koichim <koichi@masuda.sppd.ne.jp>
2025-06-29 00:42:02 +0900

    lookup gdrive

--- Traxsource2Juno.user.js
+++ Traxsource2Juno.user.js
@@ -1,17 +1,20 @@
 // ==UserScript==
 // @name         Traxsource2Juno
+// @version      0.51
 // @namespace    Traxsource2Juno
 // @match      https://www.traxsource.com/*
 // @match      https://www.junodownload.com/*
+// @match      https://masuda.sppd.ne.jp/traxsource2juno/*
 // @require 　　 https://code.jquery.com/jquery-2.0.0.min.js
 // @require     https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js
 // @downloadURL   https://github.com/koichim/Tracksource2Juno/raw/main/Traxsource2Juno.user.js
 // @updateURL   https://github.com/koichim/Tracksource2Juno/raw/main/Traxsource2Juno.user.js
 // @run-at 　　　document-end
-// @grant        GM.xmlHttpRequest
-// @grant        GM.openInTab
+// @grant        GM_xmlhttpRequest
+// @grant        GM_openInTab
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @author       Koichi Masuda
-// @version      0.41
 // @description replace artist link of Traxsource to Juno's artist search
 // ==/UserScript==
 
@@ -26,6 +29,54 @@
     var the_chart = {date:"", chart_artist:"", chart_title:"", chart_url:"", chart:[]};
     var OPEN_TAB_INTERVAL = 3000; // in ms
     var debug=1;
+    var my_mp3_tracks = {};
+    var CLIENT_ID = "635773455630-prbo14scv7350e7juhmrl8nfu8e9pa9p.apps.googleusercontent.com";
+    var GD_TOKEN_REFRESH_BEFORE_MIN = 10; // refresh 10 min beore expires
+    var GD_ready_promise = new $.Deferred();
+    var gdrive_icon_url="https://fonts.gstatic.com/s/i/productlogos/drive_2020q4/v8/web-64dp/logo_drive_2020q4_color_2x_web_64dp.png";
+
+
+
+   function invoke_http_request(dbg_str, url, data, method, headers, funcs){
+       GM_xmlhttpRequest({
+           // synchronous: true, //同期処理のためのオプションだが、機能しなかったのでコメントアウトした。
+           method: method,
+           url: url,
+           data: data,
+           headers: headers,
+           nocache: true,
+           onload: function (response) {
+               let resp_json = JSON.parse(response.responseText);
+               console.log(dbg_str+": GM_xmlHttpRequest onload");
+               if (funcs && funcs.onload) {funcs.onload(response);}
+           },
+/*           onprogress:function(response){
+               console.log(dbg_str+": GM_xmlHttpRequest onprogress");
+               console.log(response);
+               if (funcs && funcs.onprogress) {funcs.ongrogress(response);}
+           },
+           onreadystatechange:function(response){
+               console.log(dbg_str+": GM_xmlHttpRequest onreadystatechange");
+               console.log(response);
+               if (funcs && funcs.onreadystatechange) {funcs.onreadystatechange(response);}
+           },*/
+           onerror: function(response){
+               console.log(dbg_str+": GM_xmlHttpRequest error");
+               console.log(response);
+              if (funcs && funcs.onerror) { funcs.onerror(response);}
+           },
+           onabort:function(response){
+               console.log(dbg_str+": GM_xmlHttpRequest aborted");
+               console.log(response);
+               if (funcs && funcs.onabort) {funcs.onabort(response);}
+           },
+           ontimeout:function(response){
+               console.log(dbg_str+": GM_xmlHttpRequest ontimeout");
+               console.log(response);
+               if (funcs && funcs.ontimeout) {funcs.ontimeout(response);}
+           }
+       });
+   }
 
     String.prototype.clean =function() {
         return(
@@ -114,37 +165,6 @@
         return {"the_mp3_file":the_best_mp3_file, "score":score, "hit_ratio":hit_ratio};
     }
 
-    var my_mp3_tracks = {};
-    GM.xmlHttpRequest({
-        // synchronous: true, //同期処理のためのオプションだが、機能しなかったのでコメントアウトした。
-        method: 'GET',
-        url: 'https://masuda.sppd.ne.jp/etc/my_mp3_tracks.json',
-        nocache: true,
-        onload: function (response) {
-            my_mp3_tracks = JSON.parse(response.responseText);
-        },
-        onprogress:function(response){
-            console.log("GM.xmlHttpRequest: onprogress");
-            console.log(response);
-        },
-        onreadystatechange:function(response){
-            console.log("GM.xmlHttpRequest: onreadystatechange");
-            console.log(response);
-        },
-        onerror: function(response){
-            console.log("GM.xmlHttpRequest: error");
-            console.log(response);
-        },
-        onabort:function(response){
-            console.log("GM.xmlHttpRequest: aborted");
-            console.log(response);
-        },
-        ontimeout:function(response){
-            console.log("GM.xmlHttpRequest: ontimeout");
-            console.log(response);
-        }
-    });
-
     $.fn.textNodeText = function() {
         let result = "";
         $(this).contents().each(function() {
@@ -157,6 +177,8 @@
 
 
     function trxsrc_run(){
+        GD_set_up_and_run();
+
         var juno_search_links = [];
         $("a.com-artists").each(function(idx, elm){
             let a = $(elm);
@@ -199,7 +221,7 @@
                         if (0<i){artist += ", ";}
                         artist += $(artist_elms[i]).text().clean();
                     }
-                    
+
                     look4mp3_results = [];
                     look4mp3_results.push(look_for_mp3(artist, title, version, true));
                     //look4mp3_results.push(look_for_mp3(artist, title, "", true)); // comment out since no version mp3 has hit with track with verion
@@ -222,9 +244,10 @@
                         let mp3_file_div = $(num_elm).parent().clone();
                         mp3_file_div.html("["+String(Math.trunc(hit_ratio*100))+"%] "+the_mp3_file);
                         let mp3_file_color = '#ccc';
-                        if (1.0 <= hit_ratio) {
+                        if (0.9 <= hit_ratio) {
                             let num_div= $(num_elm).children("div");
-                            $(num_div).html("<span title=\""+the_mp3_file+"\">&#x2714;</span>"+$(num_div).html()); // check mark
+                            //$(num_div).html("<span title=\""+the_mp3_file+"\">&#x2714;</span>"+$(num_div).html()); // check mark
+                            $(num_div).html("&#x2714;"+$(num_div).html()); // check mark
                             $(num_elm).parent().find('*').css({'color':'#707070'}); // gray out
                             mp3_file_color = '#707070';
                             if (1 != hit_ratio) {
@@ -250,7 +273,7 @@
                         mp3_file_div.attr('id', "Tracksource2Juno: "+the_mp3_file);
 
                     }
-                    if (hit_ratio < 1.0 && num <= 10) {
+                    if (hit_ratio < 0.9 && num <= 10) {
                         if (artist_elms && artist_elms[0] && artist_elms[0].href) {
                             juno_search_links.push(artist_elms[0].href); // collect juno search links if no mp3 found
                         }
@@ -259,6 +282,9 @@
                     let the_mp3_file_4json = the_mp3_file;
                     if (hit_ratio < 0.8) {
                         the_mp3_file_4json = ""; // regard as not found
+                    }
+                    if (hit_ratio != 1 && num <= 10){
+                        GD_search(artist + " " + title + " " + version, num_elm);
                     }
                     the_chart.chart[Number(num-1)] = {num: num, title:title, version:version, artist:artist, mp3_file:the_mp3_file_4json};
                 }
@@ -298,6 +324,169 @@
         }
     }
 
+    function GD_poll_ready(){
+        let GD_interval_id = setInterval(() => {
+            let GD_auth_code = GM_getValue("GD_auth_code");
+            let now = new Date();
+            if (GD_auth_code && now.getTime() + GD_TOKEN_REFRESH_BEFORE_MIN*60*1000 < GD_auth_code.expires_at) {
+                // OK, go ahead!
+                console.log("got GD auth_code");
+                console.log(GD_auth_code);
+                clearInterval(GD_interval_id);
+                $("#Traxsource2Juno_enableGDsearch").css({filter:"grayscale(0%)"});
+                GD_ready_promise.resolve();
+            }
+        },CHECK_INTERVAL);
+
+    }
+    function GD_set_up_and_run(){
+        if (!$("#Traxsource2Juno_enableGDsearch")[0]){
+            if ($("#bodyBay")[0] && $("#bodyBay").find(".sharebox-inline")){
+                $("#bodyBay").find(".sharebox-inline").append(
+                    "<img id=\"Traxsource2Juno_enableGDsearch\" "+
+                    "style=\"\overflow:visible; width:26px; height:26px; filter:grayscale(100%);\" "+
+                    "src=\""+gdrive_icon_url+"\"/>");
+                GD_poll_ready();
+                $("#Traxsource2Juno_enableGDsearch").on("click", () => {
+                    GD_get_auth_code_in_tab();
+                    $.when(GD_ready_promise).done(()=>GD_process());
+                });
+            }
+        }
+    }
+    function GD_search(search_str, num_elm){
+        console.log(search_str);
+        let words = artist_title_cleansing_array(search_str, true);
+        let q = "";
+        words.forEach((a_word) => {
+            if (q) {q+=" and ";}
+            q += "name contains '" + a_word+"'";
+        });
+        q += " and mimeType = 'audio/mpeg'";
+        console.log("GD_search: "+q);
+        q = encodeURIComponent(q);
+        $.when(GD_ready_promise).done(()=>{
+            /* https://www.mii47.com/entry/2025/03/09/212911
+        curl -X GET 'https://www.googleapis.com/drive/v3/files?q='"'"'[フォルダID]'"'"'+in+parents+and+trashed=false&supportsAllDrives=true&includeItemsFromAllDrives=true' \
+        -H "Authorization: Bearer [トークン]"
+        google reference: https://developers.google.com/workspace/drive/api/reference/rest/v3/files/list?hl=ja
+        ファイルやフォルダを検索する - https://developers.google.com/workspace/drive/api/guides/search-files?hl=ja
+        */
+            let GD_auth_code = GM_getValue("GD_auth_code");
+            let folder_id = "1unILTVnIhi3VsrSWFrOomNAlVM-Fqu9v"; // music_backup folder
+            let headers = {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer "+GD_auth_code.access_token,
+            }
+            let funcs = {onload: (res) => {
+                res_json = JSON.parse(res.responseText);console.log(res_json);
+                if (res_json.files.length) {
+                    $(num_elm).css({"background-image": "url('"+gdrive_icon_url+"')",
+                                   "background-size": "contain",
+                                   "background-repeat": "no-repeat",
+                                   "background-position": "center",
+                                   "mix-blend-mode": "exclusion"});
+                    $(num_elm).attr("title",res_json.files[0].name);
+
+                }
+            },
+                        };
+            invoke_http_request("GD_process",
+                                "https://www.googleapis.com/drive/v3/files?q="+q,
+                                "",
+                                "GET",
+                                headers,
+                                funcs);
+        });
+    }
+
+    function GD_process(){
+        let q = " mimeType = 'audio/mpeg'";
+        q = encodeURIComponent(q);
+        let GD_auth_code = GM_getValue("GD_auth_code");
+        let folder_id = "1unILTVnIhi3VsrSWFrOomNAlVM-Fqu9v"; // music_backup folder
+        let headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer "+GD_auth_code.access_token,
+        }
+        let funcs = {onload: (res) => {res_json = JSON.parse(res.responseText);console.log(res_json);},
+                    };
+        invoke_http_request("GD_process",
+                            "https://www.googleapis.com/drive/v3/files?q="+q,
+                            "",
+                            "GET",
+                            headers,
+                            funcs);
+    }
+    function GD_get_auth_code_in_tab(){
+       /* https://accounts.google.com/o/oauth2/auth?client_id=[クライアントID]&
+          redirect_uri=https://localhost:8080&
+          scope=https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.metadata&
+          response_type=code&
+          access_type=offline
+        */
+        //GM_setValue("GD_auth_code",""); // debug
+        let GD_auth_code = GM_getValue("GD_auth_code");
+        let now = new Date();
+        if (GD_auth_code && now.getTime() + GD_TOKEN_REFRESH_BEFORE_MIN*60*1000 < GD_auth_code.expires_at) {
+            // OK, go ahead!
+            console.log("already have GD auth_code="+GD_auth_code);
+            return;
+        }
+        GM_setValue("GD_auth_code","");
+        let redirect_uri = encodeURIComponent("https://masuda.sppd.ne.jp/traxsource2juno/");
+        let scope = encodeURIComponent("https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.metadata.readonly");
+        let url = " https://accounts.google.com/o/oauth2/v2/auth?client_id="+CLIENT_ID+"&"+
+            "redirect_uri="+redirect_uri+"&"+
+            "scope="+scope+"&"+
+            "response_type=token";
+            //"&access_type=offline";
+        console.log("GD_move_and_get_auth_code: open google auth page");
+        let tab_control = GM_openInTab(url);
+        let GD_interval_id = setInterval(() => {
+            GD_auth_code = GM_getValue("GD_auth_code");
+            if (GD_auth_code) {
+                console.log("got GD auth_code");
+                console.log(GD_auth_code);
+                clearInterval(GD_interval_id);
+                tab_control.close();
+                // OK, go ahead!
+            }
+        },CHECK_INTERVAL);
+
+    }
+
+    /*
+    * masuda.sppd page
+    */
+    function getParam(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[#?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+    function GD_get_auth_code_run(){
+        /* http://masuda.sppd.ne.jp/traxsource2juno/?code=[Authorization Code]&
+           scope=https://www.googleapis.com/auth/drive%20https://www.googleapis.com/auth/drive.metadata
+         */
+        let auth_code = {access_token: "",
+                         expires_in: "",
+                         scope:""};
+        $.each(auth_code, (key, value) => auth_code[key] = getParam(key));
+        if (!auth_code.access_token){
+            $("body").html("getting auth code failed.<br/>url is "+window.location.href);
+        }
+        auth_code.expires_at = new Date().getTime() + auth_code.expires_in*1000;
+        console.log("got GD auth_code");
+        console.log(auth_code);
+        GM_setValue("GD_auth_code", auth_code);
+    }
+    /*
+    * Juno page
+    */
     function add_trxsrc_link(parent_node, search_text){
         let search_title = search_text.replace(/\s+/g, "+");
         parent_node.append(" <a href=\"https://www.traxsource.com/search?term="+search_title+"\"><img style=\"width:23px; height:23px; filter: grayscale(100%);\"src=\"https://www.traxsource.com/logos-and-images/logo-icon.png\"/></a>");
@@ -319,12 +508,57 @@
         }
     }
 
+    if (location.href.match(/masuda.sppd.ne.jp\/traxsource2juno\//i)){
+        GD_get_auth_code_run();
+    } else if (location.hostname.match(/traxsource.com$/i)){
+        let funcs = {
+            onload: (response) => {
+                my_mp3_tracks = JSON.parse(response.responseText);
+            }
+        }
+        invoke_http_request("getting my_mp3_tracks",
+                     "https://masuda.sppd.ne.jp/etc/my_mp3_tracks.json",
+                     "", // data
+                     "GET",
+                     {"Content-Type": "application/json"}, //headers
+                     funcs);
+    }
+/*
+    GM.xmlHttpRequest({
+        // synchronous: true, //同期処理のためのオプションだが、機能しなかったのでコメントアウトした。
+        method: 'GET',
+        url: 'https://masuda.sppd.ne.jp/etc/my_mp3_tracks.json',
+        nocache: true,
+        onload: function (response) {
+            my_mp3_tracks = JSON.parse(response.responseText);
+        },
+        onprogress:function(response){
+            console.log("GM.xmlHttpRequest: onprogress");
+            console.log(response);
+        },
+        onreadystatechange:function(response){
+            console.log("GM.xmlHttpRequest: onreadystatechange");
+            console.log(response);
+        },
+        onerror: function(response){
+            console.log("GM.xmlHttpRequest: error");
+            console.log(response);
+        },
+        onabort:function(response){
+            console.log("GM.xmlHttpRequest: aborted");
+            console.log(response);
+        },
+        ontimeout:function(response){
+            console.log("GM.xmlHttpRequest: ontimeout");
+            console.log(response);
+        }
+    });
+*/
     var interval_id = setInterval(function(){
         if (location.hostname.match(/traxsource.com$/i)){
             trxsrc_run();
         } else if (location.hostname.match(/junodownload.com$/i)){
             juno_run();
         }
-
     },CHECK_INTERVAL);
 //})();
